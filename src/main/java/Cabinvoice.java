654591919e3eba2bfc8afa98da50cae834ba2a5b
saiteja-code <saitejakanaparthy@gmail.com>
2020-10-30 22:42:38 +0530

    Step-4

--- src/main/java/Cabinvoice.java
+++ src/main/java/Cabinvoice.java
@@ -1,8 +1,25 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Cabinvoice {
     int Total=0;
-    int No_of_rides=0;
+    int No_ofrides=0;
+    public HashMap<String, ArrayList<Integer>>rides=new HashMap<>();
+    public ArrayList<Integer>FareDetails;
+    public void faredetail(String Id,int distance,int time){
+        int TotalFare=invoiceGenerator(distance,time);
+        System.out.println("\nTotal Fare : "+TotalFare);
+        No_ofrides++;
+        ArrayList<Integer> rideDetails=new ArrayList<>();
+        if(rides.containsKey(Id)){
+            rideDetails=rides.get(Id);
+        }
+        rideDetails.add(TotalFare);
+        rides.put(Id,rideDetails);
+        System.out.println(rides);
+    }
      public int invoiceGenerator(int distance,int time){
-         No_of_rides++;
+
         int cost=0;
         cost =distance*10+1*time;
         if(cost<5){
@@ -13,8 +30,31 @@ public class Cabinvoice {
         }
         return Total;
     }
-    public double AvgFare(){
-         double Avg=Total/No_of_rides;
-         return Avg;
+    public Invoicedetails RideRepository(String Id) {
+        FareDetails = rides.get(Id);
+        int totalfare = 0;
+        for(int fare:FareDetails){
+            totalfare = totalfare + fare;
+    }
+        int totalRides=FareDetails.size();
+        double avg=(double)totalfare/(double)totalRides;
+        return new Invoicedetails(Id, totalfare, totalRides, avg);
+    }
+    class Invoicedetails{
+         int total;
+         int No_of_rides;
+         String Id;
+         double averagefare;
+         Invoicedetails(String Id,int total, int No_of_rides,double avearagefare){
+             this.Id=Id;
+             this.total=total;
+             this.No_of_rides=No_of_rides;
+             this.averagefare=avearagefare;
+         }
+         @Override
+         public String toString(){
+             return "UserId : " + Id + "\nTotal Fare : " + total +
+                     "\nTotal number of Rides: " + No_of_rides + "\nAverage Amount : " + averagefare;
+         }
     }
 }
--- src/test/java/CabInvoiceTest.java
+++ src/test/java/CabInvoiceTest.java
@@ -5,11 +5,13 @@ public class CabInvoiceTest {
     @Test
     public void shouldReturnTotalFareOfJourney() {
         Cabinvoice cb=new Cabinvoice();
-        cb.invoiceGenerator(0,6);
-        cb.invoiceGenerator(0,3);
-        cb.invoiceGenerator(2,5);
-        Assert.assertEquals(36,cb.Total);
-        Assert.assertEquals(3, cb.No_of_rides);
-        Assert.assertEquals(12,cb.AvgFare(),0);
+        cb.faredetail("Sai", 4, 2);
+        cb.faredetail("Sai", 7, 2);
+        cb.faredetail("Sai", 2, 8);
+
+        int Total = cb.RideRepository("Sai").total;
+        System.out.println(Total);
+        Assert.assertEquals(142, Total);
+
     }
 }
