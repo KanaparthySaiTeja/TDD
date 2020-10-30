7c86c620dbcfcf8306f80e03c2a98f2c244f4a37
saiteja-code <saitejakanaparthy@gmail.com>
2020-10-30 20:31:23 +0530

    Step-3

--- src/main/java/Cabinvoice.java
+++ src/main/java/Cabinvoice.java
@@ -1,6 +1,8 @@
 public class Cabinvoice {
     int Total=0;
+    int No_of_rides=0;
      public int invoiceGenerator(int distance,int time){
+         No_of_rides++;
         int cost=0;
         cost =distance*10+1*time;
         if(cost<5){
@@ -10,5 +12,9 @@ public class Cabinvoice {
             Total = Total + cost;
         }
         return Total;
+    }
+    public double AvgFare(){
+         double Avg=Total/No_of_rides;
+         return Avg;
     }
 }
--- src/test/java/CabInvoiceTest.java
+++ src/test/java/CabInvoiceTest.java
@@ -9,5 +9,7 @@ public class CabInvoiceTest {
         cb.invoiceGenerator(0,3);
         cb.invoiceGenerator(2,5);
         Assert.assertEquals(36,cb.Total);
+        Assert.assertEquals(3, cb.No_of_rides);
+        Assert.assertEquals(12,cb.AvgFare(),0);
     }
 }
