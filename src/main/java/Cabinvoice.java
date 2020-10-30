53d4eaae95474e2106830e3c6ab02505598ec90d
saiteja-code <saitejakanaparthy@gmail.com>
2020-10-30 18:54:48 +0530

    Step-2-MultipleRides

--- src/main/java/Cabinvoice.java
+++ src/main/java/Cabinvoice.java
@@ -1,11 +1,14 @@
 public class Cabinvoice {
+    int Total=0;
      public int invoiceGenerator(int distance,int time){
         int cost=0;
         cost =distance*10+1*time;
         if(cost<5){
-            return 5;
+            Total=Total+5;
         }
-        
-        return cost;
+        else {
+            Total = Total + cost;
+        }
+        return Total;
     }
 }
--- src/test/java/CabInvoiceTest.java
+++ src/test/java/CabInvoiceTest.java
@@ -5,7 +5,9 @@ public class CabInvoiceTest {
     @Test
     public void shouldReturnTotalFareOfJourney() {
         Cabinvoice cb=new Cabinvoice();
-        int Fare= cb.invoiceGenerator(0,6);
-        Assert.assertEquals(6,Fare);
+        cb.invoiceGenerator(0,6);
+        cb.invoiceGenerator(0,3);
+        cb.invoiceGenerator(2,5);
+        Assert.assertEquals(36,cb.Total);
     }
 }
